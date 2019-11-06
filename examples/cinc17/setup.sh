b81aa489fe9a93c25aeb92d3d105a55ffedc0910
Fabian Rabe <fabian.rabe@informatik.uni-augsburg.de>
2019-11-06 15:52:49 +0100

    fixed URL, setup script

--- examples/cinc17/setup.sh
+++ examples/cinc17/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-url=https://www.physionet.org/challenge/2017/
+url=https://archive.physionet.org/challenge/2017/
 
-mkdir data && cd data
+mkdir data 
+cd data
 
 curl -O $url/training2017.zip
 unzip training2017.zip
